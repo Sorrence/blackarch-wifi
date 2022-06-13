9f12ea12a1173219db897173a61f758c7da869f8
Sorrence <82121344+Sorrence@users.noreply.github.com>
2022-06-13 23:10:25 +0000

    Update wifi-connect.sh

--- wifi-connect.sh
+++ wifi-connect.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 iwctl device list
-echo "Select an interface to connect wifi"
+echo "Select an interface to connect wifi:"
+echo ""
 read interface
 
 iwctl station $interface get-networks
-echo "Select a wifi to connect (You can copy-paste with Ctrl+Alt+C and Ctrl+Alt+V)"
+echo "Select a wifi to connect (You can copy-paste with Ctrl+Alt+C and Ctrl+Alt+V):"
+echo ""
 read wifi
 
 iwctl station $interface connect $wifi
